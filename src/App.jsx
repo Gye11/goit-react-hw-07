7963e595b782a1d9ed2305b02b7d22af75216236
Gye11 <hilallornekk@gmail.com>
2026-03-31 01:21:38 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
 import ContactsForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
+
+import { fetchContacts } from "./redux/contactsOps";
+
 import "./App.css";
 
 function App() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state) => state.contacts.loading);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div className="app">
       <h1 className="title">📱 Phonebook</h1>
 
       <ContactsForm />
       <SearchBox />
+
+      {loading && <p className="status">Loading...</p>}
+
       <ContactList />
     </div>
   );
